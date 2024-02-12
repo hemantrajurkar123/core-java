9ed05ac1dae80049d8422a614d07133748d5d5d1
Hemant <hemant.rajurkar123@gmail.com>
2024-02-13 00:23:14 +0530

    if condition started

---
+++ src/conditional_statements/IfCondition.java
@@ -0,0 +1,16 @@
+package conditional_statements;
+
+public class IfCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int person_age=20;
+		if(person_age>19) {
+			System.out.println("Eligible for vote");
+		}else {
+			System.out.println("Not eligible for vote");
+		}
+
+	}
+
+}
