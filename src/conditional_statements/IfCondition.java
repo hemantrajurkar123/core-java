bafe510276e8ec3ba831bf3e48e8339ce70d54c5
Hemant <hemant.rajurkar123@gmail.com>
2024-09-29 16:01:05 +0530

    if condition

--- src/conditional_statements/IfCondition.java
+++ src/conditional_statements/IfCondition.java
@@ -4,10 +4,10 @@ public class IfCondition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int person_age=20;
-		if(person_age>19) {
+		int person_age = 20;
+		if (person_age > 19) {
 			System.out.println("Eligible for vote");
-		}else {
+		} else {
 			System.out.println("Not eligible for vote");
 		}
 
