bf9732fd68dbe04a5082099b4b58c762752a7691
Hemant <hemant.rajurkar123@gmail.com>
2024-09-30 11:57:02 +0530

    shortcuts for comments

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,25 +1,34 @@
 
+//public class HelloWorld {
+//	
+//	public static void displayString(String x){
+//		System.out.println(x);
+//	}
+//
+//	public static void main(String[] args) {
+//		System.out.println("Hello World From JAVA");
+//		displayString("hemant is here");
+//		System.out.println(10+20);
+//	
+//		
+//	}
+//
+//}
+
 public class HelloWorld {
-	
-	public static void displayString(String x){
+	public static void displayString(String x) {
 		System.out.println(x);
 	}
-
-	public static void main(String[] args) {
-		System.out.println("Hello World From JAVA");
-		displayString("hemant is here");
-		System.out.println(10+20);
-	
-		
+	public static void main(String args[]) {
+		displayString("Hemant is in Germany");
+		System.out.println("He is going to be a millionaire");
 	}
-
 }
 
 //Learnings:
 //1. A java program must have a class and the main method. Without it we can not run a program
 //2. A class name should always start with an uppercase letter
 //3. All the programs in java will have src as its root folder. You can add package (folder) inside the src folder and
- //  your java classes inside that package.
+// your java classes inside that package.
 //4. A package is just like a folder. You can have multiple packages inside src
 //5. The shortcut for the System.out.println statement is Syso +CTRL+SPACE BAR
-	
