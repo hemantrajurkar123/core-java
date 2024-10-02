ed14d438b3914e0223dc8b83b6a44c5b30a54ca5
Hemant <hemant.rajurkar123@gmail.com>
2024-10-02 17:29:34 +0530

    data types notes added

--- src/data_types/VariablesDemo.java
+++ src/data_types/VariablesDemo.java
@@ -22,7 +22,31 @@ public class VariablesDemo {
 		System.out.println(f+g+h); // it will print the addition
 		System.out.println(f+" "+g+" "+h); // it will print the respective values of the variable
 		
-		byte j=128;
-	}
+		// data types
+		
+		// primitive data types- Those which are predefined there are 8 primitive data type
+		// numeric data types----> byte, short, int, long
+		// float data types----> float, double
+		// boolean and char
+		
+		// These data types are provided because every data type has range. If we define int data type with the value of say x=123, it will be a waste of memory
+		// We can not directly print the value of any variable without declaring it with some value
+		
+		byte k = 0;
+		System.out.println("byte x"+k);
+		
+		boolean x=true;
+		System.out.println("boolean value "+x);
+		
+		// Non primitive data types--> Not predefined in java except Strings. Others are created by developers
+		// String, Arrays, Classes, Interfaces are non primitive data types
+		// A primitive type always has a value, while non-primitive types can be null.
+		
+		// The char data type holds the single char. We have to use the single quote for its value
+		char grade='B';
+		
+		// We have to use double quote to define the string value as string contains multiple characters
+		String s="String";
+		}
 
 }
