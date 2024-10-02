6f2169115169bdf9887e1a875922c3247a3841bb
Hemant <hemant.rajurkar123@gmail.com>
2024-10-02 16:56:09 +0530

    data types in java

---
+++ src/data_types/VariablesDemo.java
@@ -0,0 +1,28 @@
+package data_types;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a; //declaration
+		a=100; // assignment
+		System.out.println("The value of a is "+a);
+		
+		int b=200; //declaration+assignment
+		System.out.println("The value of b is  "+b);
+		
+		// For multiple variables
+		int c,d,e;  // declaring multiple variables
+		c=300;
+		d=400;
+		e=500;
+		
+		// defining multiple variables in a single line
+		int f=300,g=400,h=500;
+		System.out.println(f+g+h); // it will print the addition
+		System.out.println(f+" "+g+" "+h); // it will print the respective values of the variable
+		
+		byte j=128;
+	}
+
+}
